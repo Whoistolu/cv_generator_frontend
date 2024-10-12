37e4c7cde7dfc02ee3869e71606a6ddca71953a2
Whoistolu <digittolu25@gmail.com>
2024-10-12 22:46:02 +0100

    Fix the AdminDashboard component

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import TemplateList from './TemplateList';
 
 const AdminDashboard = () => {
   return (
-    <div className="container p-4 mx-auto">
-      <h1 className="mb-4 text-2xl font-bold">Admin Dashboard</h1>
-      <TemplateList />
+    <div>
+
     </div>
   );
 };
