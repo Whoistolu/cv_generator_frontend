7f100a1101b885241d3f2d9c510172ac9d0b4127
Whoistolu <digittolu25@gmail.com>
2024-10-13 19:47:36 +0100

    Add AdminDashboardPage component

---
+++ src/pages/AdminDashboardPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminDashboardPage = () => {
+ 
+  return (
+    <div className="template-list">
+      
+    </div>
+  );
+};
+
+export default AdminDashboardPage;
