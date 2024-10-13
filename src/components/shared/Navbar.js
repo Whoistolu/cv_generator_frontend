8c96cc87771017e5ce80f19f6c425855c78be2b3
Whoistolu <digittolu25@gmail.com>
2024-10-13 23:51:21 +0100

    Adjust navbar style

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -5,16 +5,15 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
 
     return (
-        <nav className="fixed top-0 z-50 w-full h-16 bg-blue-800 shadow-md">
+        <nav className="fixed top-0 z-50 w-full h-16 bg-blue-600 shadow-md">
             <div className='container flex items-center justify-between h-full px-4 mx-auto'>
                 <div className="text-2xl font-bold text-white">
                     <div> CV GENERATOR</div>
                 </div>
 
                 <div className="hidden space-x-6 md:flex">
-                    <div className="text-white hover:text-gray-900">Home</div>
-                    <div className="text-white hover:text-gray-900">About</div>
-                    <div className="text-white hover:text-gray-900">Contact</div>
+                    <div className="text-white hover:text-red-900">REGISTER</div>
+                    <div className="text-white hover:text-red-900">LOGIN</div>
                 </div>
             </div>
         </nav>
