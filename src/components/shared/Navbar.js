e44e4167a8292a267405b967bae3bf4e8230a6ad
Whoistolu <digittolu25@gmail.com>
2024-10-13 23:43:42 +0100

    Styled the Navbar component

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
- 
-  return (
-    <div className="template-list">
-      
-    </div>
-  );
+
+    return (
+        <nav className="fixed top-0 z-50 w-full h-16 bg-blue-800 shadow-md">
+            <div className='container flex items-center justify-between h-full px-4 mx-auto'>
+                <div className="text-2xl font-bold text-white">
+                    <div> CV GENERATOR</div>
+                </div>
+
+                <div className="hidden space-x-6 md:flex">
+                    <div className="text-white hover:text-gray-900">Home</div>
+                    <div className="text-white hover:text-gray-900">About</div>
+                    <div className="text-white hover:text-gray-900">Contact</div>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
