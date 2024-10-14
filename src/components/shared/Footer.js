aefc0a642142d197a8c794215d8a17d512c0182c
Whoistolu <digittolu25@gmail.com>
2024-10-14 21:09:03 +0100

    Add extra styles

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
     <footer className="w-full py-4 text-white bg-gray-800">
       <div className="container flex items-center justify-between px-4 mx-auto">
-        
+        <div className="text-sm">
+          © {new Date().getFullYear()} CV Generator. All rights reserved.
+        </div>
       </div>
     </footer>
   );
