9cb3da2bc5637f89d24c7e1b3b8fc6188f0e5d20
Whoistolu <digittolu25@gmail.com>
2024-10-14 21:16:55 +0100

    Render the Footer coponent in the AdminDashboard component

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../shared/Navbar';
+import Footer from '../shared/Footer';
 
 const AdminDashboard = () => {
   return (
     <div>
         <Navbar />
+        <Footer />
     </div>
   );
 };
