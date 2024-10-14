7ec222f3fbce4ba06ee46bdb54e2f9ffd90e2a64
Whoistolu <digittolu25@gmail.com>
2024-10-14 21:08:31 +0100

    Add more styles

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -4,7 +4,9 @@ const Footer = () => {
  
   return (
     <footer className="w-full py-4 text-white bg-gray-800">
-      
+      <div className="container flex items-center justify-between px-4 mx-auto">
+        
+      </div>
     </footer>
   );
 };
