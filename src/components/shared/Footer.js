c126703e6123d433336dccf7dfff5770c1300484
Whoistolu <digittolu25@gmail.com>
2024-10-14 21:30:59 +0100

    Add some implementations to the FromInput component

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Footer = () => {
  
   return (
-    <footer className="w-full py-4 text-white bg-gray-800">
+    <footer className="w-full h-16 py-4 text-white bg-gray-800">
       <div className="container flex items-center justify-between px-4 mx-auto">
         <div className="text-sm">
-          © {new Date().getFullYear()} CV Generator. All rights reserved.
+          © {new Date().getFullYear()} CV GENERATOR. All rights reserved.
         </div>
 
         <div className="flex space-x-4">
--- src/components/shared/FormInput.js
+++ src/components/shared/FormInput.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const FormInput = () => {
- 
+const FormInput = ({ label, type, name, value, onChange, placeholder }) => {
   return (
-    <div className="template-list">
-      
+    <div className="mb-4">
+      <label htmlFor={name} className="block text-sm font-medium text-gray-700">
+        {label}
+      </label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+       
+      />
     </div>
   );
 };
