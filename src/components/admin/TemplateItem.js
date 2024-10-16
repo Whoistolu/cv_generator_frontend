8a0aea4fcf2f29f4b551439b37e6ac3914271403
Whoistolu <digittolu25@gmail.com>
2024-10-16 23:28:13 +0100

    Refactored the code for the Template items

--- src/components/admin/TemplateItem.js
+++ src/components/admin/TemplateItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const TemplateItem  = () => {
+const TemplateItem  = ({name}) => {
   return (
-    <div>
-
+    <div className="p-4 bg-white rounded-md shadow-md">
+      <h3 className="text-xl font-semibold">{name}</h3>
+      <div className="flex justify-between mt-4">
+        <button className="text-blue-500 hover:underline">Edit</button>
+        <button className="text-red-500 hover:underline">Delete</button>
+      </div>
     </div>
   );
 };
--- src/components/user/CVTemplateItem.js
+++ src/components/user/CVTemplateItem.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-const CVTemplateItem = ({ name }) => {
+const CVTemplateItem = () => {
   return (
-    <div className="p-4 bg-white rounded-md shadow-md">
-      <h3 className="text-xl font-semibold">{name}</h3>
-      <div className="flex justify-between mt-4">
-        <button className="text-blue-500 hover:underline">Edit</button>
-        <button className="text-red-500 hover:underline">Delete</button>
-      </div>
+    <div>
+
     </div>
   );
 };
