096c797176a604d6d676013faa4bc4b1ca172c6b
Whoistolu <digittolu25@gmail.com>
2024-10-16 22:55:16 +0100

    ReStyle the Footer component

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Navbar from '../shared/Navbar';
 import Footer from '../shared/Footer';
+import FormInput from '../shared/FormInput';
 
 const AdminDashboard = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
 
-      <div className="flex-grow pt-16">
+      <div className="flex-grow pt-16 text-center">
         <h1>Welcome to the Admin Dashboard</h1>
       </div>
+      <FormInput />
       <Footer />
     </div>
   );
--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -3,16 +3,16 @@ import React from 'react';
 const Footer = () => {
  
   return (
-    <footer className="w-full h-16 py-4 text-white bg-gray-800">
+    <footer className="w-full h-16 py-4 text-white bg-blue-600">
       <div className="container flex items-center justify-between px-4 mx-auto">
         <div className="text-sm">
           © {new Date().getFullYear()} CV GENERATOR. All rights reserved.
         </div>
 
         <div className="flex space-x-4">
-          <div href="#" className="hover:text-blue-400">Privacy Policy</div>
-          <div href="#" className="hover:text-blue-400">Terms of Service</div>
-          <div href="#" className="hover:text-blue-400">Contact Us</div>
+          <div href="#" className="hover:text-red-900">Privacy Policy</div>
+          <div href="#" className="hover:text-red-900">Terms of Service</div>
+          <div href="#" className="hover:text-red-900">Contact Us</div>
         </div>
       </div>
     </footer>
