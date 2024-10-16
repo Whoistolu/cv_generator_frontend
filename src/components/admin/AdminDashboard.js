1097d7e78d994371d0ae21a2fea99047b70ed05e
Whoistolu <digittolu25@gmail.com>
2024-10-16 22:26:53 +0100

    Fix the errordisplay issues with the footer

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -4,9 +4,13 @@ import Footer from '../shared/Footer';
 
 const AdminDashboard = () => {
   return (
-    <div>
-        <Navbar />
-        <Footer />
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+
+      <div className="flex-grow pt-16">
+        <h1>Welcome to the Admin Dashboard</h1>
+      </div>
+      <Footer />
     </div>
   );
 };
