ca90f86b253a18d11fdf8831782210df7c64c689
Whoistolu <digittolu25@gmail.com>
2024-10-16 23:13:53 +0100

    Add more implementations to the SideBar component

--- src/components/admin/SideBar.js
+++ src/components/admin/SideBar.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Sidebar = () => {
   return (
-   <div>
-    
+   <div className='left-0 w-64 h-full bg-blue-200 shadow-md top-16'>
+        <ul className='p-4 mt-6 space-y-4'>
+            <li className="p-2 rounded hover:bg-red-700">
+                <a href="#">Manage Templates</a>
+            </li>
+            <li className="p-2 rounded hover:bg-red-700">
+                <a href="#">View Users</a>
+            </li>
+            <li className="p-2 rounded hover:bg-red-700">
+                <a href="#">Settings</a>
+            </li>
+        </ul>
    </div>
   );
 };
