bc13039826a44cbb6035a0daabf3199616e6505d
Whoistolu <digittolu25@gmail.com>
2024-10-17 22:05:46 +0100

    Add AdminOverview component

---
+++ src/components/admin/AdminOverview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminOverview = () => {
+
+    return (
+        <div>
+            <h1>Admin Overview</h1>
+        </div>
+    )
+}
+
+export default AdminOverview;
