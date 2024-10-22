5e8325b3abab473bf094a56509796e367b091634
Whoistolu <digittolu25@gmail.com>
2024-10-22 21:25:37 +0100

    Add form to the SignupPage component

--- src/components/shared/Signup.js
+++ src/components/shared/Signup.js
@@ -1,7 +1,25 @@
 const Signup = () => {
     return (
         <div>
-            
+            <form>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                />
+
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                />
+
+                <input
+                    type="pasword"
+                    name="password_confirmation"
+                    placeholder="Confirm Password"
+                />
+            </form>
         </div>
     )
 }
