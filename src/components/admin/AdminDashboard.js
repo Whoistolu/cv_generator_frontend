679d713cfc50eb7007e2ca34c8a2bfe0c9a076b5
Whoistolu <digittolu25@gmail.com>
2024-10-22 21:06:55 +0100

    Made modifications and Add a new component

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -9,12 +9,14 @@ const AdminDashboard = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-
-      <div className="flex-grow pt-16 text-center">
-        <h1>Welcome to the Admin Dashboard</h1>
+      
+      <div className="flex flex-grow pt-16">
+        <Sidebar /> 
+        <div className="flex-grow p-8 text-center">
+          <h1>Welcome to the Admin Dashboard</h1>
+          <FormInput />
+        </div>
       </div>
-      <Sidebar />
-      <FormInput />
       <Footer />
     </div>
   );
--- src/components/admin/SideBar.js
+++ src/components/admin/SideBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Sidebar = () => {
   return (
-   <div className='left-0 w-64 h-full bg-blue-200 shadow-md top-16'>
+   <div className='left-0 w-64 h-full bg-blue-100 shadow-md top-16'>
         <ul className='p-4 mt-6 space-y-4'>
             <li className="p-2 rounded hover:bg-red-700">
                 <a href="#">Manage Templates</a>
