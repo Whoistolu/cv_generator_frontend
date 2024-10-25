eb90f4dbf2425e1e8e575064fcda60085321a676
Whoistolu <digittolu25@gmail.com>
2024-10-25 11:52:56 +0100

    Update the Login component

--- src/components/shared/Login.js
+++ src/components/shared/Login.js
@@ -1,17 +1,47 @@
+import { useState } from "react"
+import axios from 'axios'
+
 const Login = () => {
+
+    const [formdata, setFormdata] = useState({
+        login: '',
+        password: ''
+
+    })
+
+    const handleChange = (e) => {
+       setFormdata({
+        ...formdata, [e.target.name]: e.target.value
+       })
+    }
+
+    const handleSubmit =  async (e) => {
+        e.preventdefault()
+
+        try {
+            const response = await axios.post('/users', formdata)
+            console.log(response)
+        } catch (error) {
+            console.error('Login error', error)
+        }
+    } 
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     type='email'
                     name='email'
                     placeholder="email"
+                    onChange={handleChange}
                 />
 
                 <input
                      type='password'
                      name='password'
                      placeholder="password"
+                    onChange={handleChange}
+
                 />
             </form>
         </div>
